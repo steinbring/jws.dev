0a3f5248942474da1f1e34a13438644a14ed2f49
Joe Steinbring <joe@jws.app>
2024-05-11 20:00:03 -0500

    Let's try this

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-npm add -D vitepress
+# Exit script on error
+set -e
 
+# Echo each command for logging
+set -x
+
+# Check out the code from the repository
+# (Assumed to be managed externally or this script is part of the repository)
+
+# Install Node.js dependencies
+echo "Installing dependencies..."
+npm install
+
+# Build the VitePress site
+echo "Building VitePress site..."
 npm run docs:build
