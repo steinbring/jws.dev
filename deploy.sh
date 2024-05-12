5f35d0bd13b2e9509db7689b843f347df4fc2868
Joe Steinbring <joe@jws.app>
2024-05-11 20:25:35 -0500

    Let's try this?

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Exit script on error
-set -e
+yarn add -D vitepress
 
-# Echo each command for logging
-set -x
-
-# Check out the code from the repository
-# (Assumed to be managed externally or this script is part of the repository)
-
-# Install Node.js dependencies
-echo "Installing dependencies..."
-npm install
-
-# Build the VitePress site
-echo "Building VitePress site..."
-npm run docs:build
+yarn docs:build
